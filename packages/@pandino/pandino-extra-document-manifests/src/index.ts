ad583ddf48fd20d06dd2e8c7826f0215dbba08b6
Norbert Herczeg <noherczeg@gmail.com>
2022-01-03 01:08:50 +0100

    update pandino-extra-document-manifests

--- packages/@pandino/pandino-extra-document-manifests/src/index.ts
+++ packages/@pandino/pandino-extra-document-manifests/src/index.ts
@@ -1,17 +1,19 @@
-import { BundleActivator, BundleContext, Fetcher, ServiceReference, FRAMEWORK_FETCHER } from '@pandino/pandino-api';
+import { BundleActivator, BundleContext, Fetcher, ServiceReference } from '@pandino/pandino-api';
 
 export default class PandinoExtraDocumentManifestsActivator implements BundleActivator {
   private fetcherReference: ServiceReference<Fetcher>;
   private fetcher: Fetcher;
 
   async start(context: BundleContext): Promise<void> {
-    this.fetcherReference = context.getServiceReference<Fetcher>(FRAMEWORK_FETCHER);
+    this.fetcherReference = context.getServiceReference<Fetcher>('@pandino/pandino/Fetcher');
     this.fetcher = context.getService<Fetcher>(this.fetcherReference);
     await this.registerDocumentDefinedManifests(context);
     return Promise.resolve();
   }
 
   stop(context: BundleContext): Promise<void> {
+    context.ungetService(this.fetcherReference);
+
     return Promise.resolve();
   }
 
@@ -19,11 +21,12 @@ export default class PandinoExtraDocumentManifestsActivator implements BundleAct
     const documentDefinedManifest = document.querySelector('script[type="pandino-manifests"]');
     let locations: string[];
     if (documentDefinedManifest.hasAttribute('src')) {
-      locations = await this.fetcher(documentDefinedManifest.getAttribute('src'));
+      locations = await this.fetcher.fetch(documentDefinedManifest.getAttribute('src'));
     } else {
       locations = documentDefinedManifest ? JSON.parse(documentDefinedManifest.textContent) : [];
     }
     await Promise.all(locations.map((manifestLocation) => context.installBundle(manifestLocation)));
+
     return Promise.resolve();
   }
 }
