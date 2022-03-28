d71740fd235e357b9f0b3e64ac3732c26c690c5f
Norbert Herczeg <noherczeg@gmail.com>
2022-03-29 01:38:02 +0200

    feat: add experimental Rollup plugin to support externalized React Component projects

---
+++ packages/@pandino/rollup-plugin-pandino-react-externalize/README.md
@@ -0,0 +1,60 @@
+# rollup-plugin-pandino-react-externalize
+
+**EXPERIMENTAL TECHNOLOGY!**
+
+Rollup plugin to generate Pandino Bundles with decoupled React Components.
+
+## Usage
+
+Example usage on creating TypeScript powered React components integrated into Pandino.
+
+```javascript
+import typescript from '@rollup/plugin-typescript';
+import { pandinoExternalizeReact } from '@pandino/rollup-plugin-pandino-react-externalize';
+import { readFileSync } from "fs";
+
+const packageJSON = JSON.parse(readFileSync('package.json').toString('utf8'));
+
+export default {
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/app-custom.mjs',
+            format: 'esm',
+        }
+    ],
+    plugins: [
+        typescript({ tsconfig: './tsconfig.json' }),
+        pandinoExternalizeReact({
+            prettify: true,
+            externalRefsMap: [
+                { token: 'useBundleContext',  identifier: '@example/app-platform-api/useBundleContext' },
+                { token: 'jsxs',              identifier: '@pandino/react-provider/react/jsx-runtime/jsxs' },
+                // { token: 'Fragment',          identifier: '@pandino/react-provider/react/jsx-runtime/Fragment' },
+                { token: 'jsx',               identifier: '@pandino/react-provider/react/jsx-runtime/jsx' },
+                // { token: 'useState',          identifier: '@pandino/react-provider/react/useState' },
+                // { token: 'Component',         identifier: '@pandino/react-provider/react/Component' },
+            ],
+            componentsMap: [
+                {
+                    component: 'CustomDashboardPageComponent',
+                    identifier: '@scope/react-ts-component-proxy/pages/Dashboard',
+                    props: {
+                        'service.ranking': 90,
+                    },
+                },
+            ],
+            manifestData: {
+                "Bundle-ManifestVersion": "1",
+                "Bundle-SymbolicName": packageJSON.name,
+                "Bundle-Name": "App Custom",
+                "Bundle-Version": packageJSON.version,
+                "Bundle-Description": packageJSON.description,
+                "Bundle-Activator": "./app-custom.mjs",
+                "Require-Capability": "app.platform;filter:=\"(type=DOM)\""
+            },
+        }),
+    ],
+};
+
+```
---
+++ packages/@pandino/rollup-plugin-pandino-react-externalize/src/index.js
@@ -0,0 +1,101 @@
+const { EOL } = require('os');
+const { sep } = require('path');
+const terserMinify = require('terser').minify;
+const prettier = require('prettier');
+
+const providerTemplate = (componentMap) => {
+  const props = typeof componentMap.props === 'object' ? JSON.stringify(componentMap.props) : '{}';
+
+  return `
+        const provide${componentMap.component} = {
+            getComponent: () => ${componentMap.component},
+            getFilter: () => undefined,
+            getIdentifier: () => '${componentMap.identifier}',
+        };
+        this.provide${componentMap.component}Registration = context.registerService(provide${componentMap.component}.getIdentifier(), provide${componentMap.component}, ${props});
+  `
+};
+
+const externalRefsTemplate = ({token, identifier}) => {
+  return `
+        this.${token}Ref = context.getServiceReference('${identifier}');
+        const ${token} = context.getService(this.${token}Ref);  
+  `;
+};
+
+const template = (componentsMap, externalRefsMap, codes) => `
+class Activator {
+    ${externalRefsMap.map(m => `${m.token}Ref`).join(EOL)}
+    ${componentsMap.map(m => `provide${m.component}Registration`).join(EOL)}
+    start(context) {
+        /**** External Refs ****/
+        ${externalRefsMap.map(externalRefsTemplate).join(EOL)}
+        
+        /**** Components ****/
+        ${codes.join(EOL)}
+        
+        /**** Register Providers ****/
+        ${componentsMap.map(providerTemplate).join(EOL)}
+
+        return Promise.resolve();
+    }
+    stop(context) {
+        ${externalRefsMap.map(m => `context.ungetService(this.${m.token}Ref);`).join(EOL)}
+        ${componentsMap.map(m => `this.provide${m.component}Registration?.unregister();`).join(EOL)}
+        return Promise.resolve();
+    }
+}
+
+export { Activator as default };
+
+`;
+
+const pandinoExternalizeReact = ({
+  componentsMap = {},
+  externalRefsMap = {},
+  minify = false,
+  prettify = false,
+  prettierConfig = {
+    parser: 'babel',
+    printWidth: 120,
+    singleQuote: true,
+    trailingComma: 'all',
+  },
+  manifestData = {},
+}) => {
+  const codes = [];
+
+  return {
+    name: 'pandino-externalize-react',
+    renderChunk: async (code, chunk, options) => {
+      for (module of Object.values(chunk.modules)) {
+        if (module.code && (module.code.startsWith('function') || module.code.startsWith('class'))) {
+          codes.push(module.code);
+        }
+      }
+    },
+    generateBundle: async (options, bundle, isWrite) => {
+      const key = Object.keys(bundle)[0];
+      const result = template(componentsMap, externalRefsMap, codes);
+      const code = prettify ? prettier.format(result, prettierConfig) : result;
+      bundle[key].code = minify ? (await terserMinify(code)).code : code;
+
+      const manifestKey = key.replace(/\.(m?)js$/, '-manifest.json');
+      const manifestFile = key
+        .substring(key.lastIndexOf(sep))
+        .replace(/\.(m?)js$/, '-manifest.json');
+
+      bundle[manifestKey] = {
+        name: manifestFile,
+        isAsset: true,
+        type: 'asset',
+        fileName: manifestFile,
+        source: JSON.stringify(manifestData, null, 4),
+      };
+    },
+  }
+}
+
+module.exports = {
+  pandinoExternalizeReact,
+};
